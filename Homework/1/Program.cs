e046c3ba83cb1927d149a9dc45e3101c40e9030b
Fennellis <westgeze@gmail.com>
2023-12-15 21:13:58 +0300

    Upd

--- Homework/1/Program.cs
+++ Homework/1/Program.cs
@@ -1,16 +1,16 @@
-﻿Circle circle = new(3);
-Console.WriteLine(circle);
+﻿// Circle circle = new(3);
+// Console.WriteLine(circle);
 
-circle.Perimetr = 10;
-System.Console.WriteLine(circle);
+// circle.Perimetr = 10;
+// System.Console.WriteLine(circle);
 
-circle.Radius = 15;
-System.Console.WriteLine(circle);
+// circle.Radius = 15;
+// System.Console.WriteLine(circle);
 
-circle.Area = 30;
-System.Console.WriteLine(circle);
+// circle.Area = 30;
+// System.Console.WriteLine(circle);
 
-System.Console.WriteLine(circle.calculateArea());
+// System.Console.WriteLine(circle.CalculateArea());
 
 // Rectangle rect = new Rectangle(2, 5);
 // System.Console.WriteLine(rect);
@@ -22,14 +22,16 @@ System.Console.WriteLine(circle.calculateArea());
 
 
 
-// Square sq = new Square(5);
-// System.Console.WriteLine(sq);
+Square sq = new Square(5);
+System.Console.WriteLine(sq);
 
-// sq.Area = 36;
-// System.Console.WriteLine(sq);
+sq.Area = 36;
+System.Console.WriteLine(sq);
 
-// sq.Length = 7;
-// System.Console.WriteLine(sq);
+sq.Length = 7;
+System.Console.WriteLine(sq);
 
-// sq.Perimetr = 48;
-// System.Console.WriteLine(sq);
+sq.Perimetr = 48;
+System.Console.WriteLine(sq);
+
+System.Console.WriteLine($"{sq.CalculateArea()} {sq.CalculatePerimeter()}");
